50c9531b6f064d52de509a362bcab7c76ca8723a
YUNHUI RAO <1617746322@qq.com>
2022-09-15 23:42:36 +1000

    Update weighted_directed_graph_generator.py

--- Assignment1/weighted_directed_graph_generator.py
+++ Assignment1/weighted_directed_graph_generator.py
@@ -6,6 +6,7 @@ import random
 #and output the result in the form: src_node dest_node capacity
 #and output the result into graph_n.txt where n is the index of the graph
 #note: the node index will be from 0 to x-1
+#note: the capacity is from 0 to 200 in a float form
 def generate_graph(number_nodes, number_graphs):
     for g in range(number_graphs):
         filename = "graph" + str(g) + ".txt"
